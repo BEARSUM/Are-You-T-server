c6c007ce70e4d4e3b47670d40a78764af2d20d51
reib13 <rebi13@naver.com>
2023-08-02 02:00:38 +0900

    feat: mbti 통계 api 추가

--- src/router/statisticRouter.js
+++ src/router/statisticRouter.js
@@ -15,15 +15,74 @@ statisticRouter.get(
   })
 );
 
-// mbti 결과 저장 (테스트 결과 정보로 처리)
+// mbti 16개 유형 전체 통계 조회
+statisticRouter.get(
+  '/',
+  asyncHandler(async (req, res, next) => {
+    const statistics = await StatisticService.getAllStatistic();
+    res.json(buildResponse(statistics));
+  })
+);
+
+// 전체 수 증가
+statisticRouter.patch('/', asyncHandler(async (req, res, next) => {
+    const allStats = await StatisticService.updateMbti(mbtiType);
+    res.json(buildResponse(allStats));
+}))
+
+// mbti 결과 저장 (테스트 결과 정보로 처리) - 테스트가 끝난 뒤 api를 호출한다. 그럼 데이터는 1번만 넣으면 되는거아님?
+// 먼저 mbti 유형부터 저장하고, 그 뒤에 상세 정보를 저장하자
 statisticRouter.put(
   '/',
   asyncHandler(async (req, res, next) => {
+
     const { parent, mbtiType, mbtiData } = req.body;
-    // mbti 전체 통계도 저장해야되는데 여기 로직에서 태워?
-    // 없으면 추가, 있으면 수정
-    const test = await StatisticService.updateStatistic({ parent, mbtiType, mbtiData });
-    res.json(buildResponse(test));
+    let createData = [];
+    let result = null;
+    const stat = await StatisticService.getStatistic(parent, mbtiType);
+    if(!stat) {
+      // 객체를 복사한 뒤 "selection" 프로퍼티를 빈 객체로 초기화
+      createData = mbtiData.map(item => {
+        return { ...item, selection: {}, };
+      });
+      // 각 객체의 "answer" 프로퍼티의 키 값을 "selection" 객체에 저장
+      createData.forEach(item => {
+        const { answer, selected, selection } = item;
+        Object.keys(answer).forEach(key => {
+          if (!selection[key]) {
+            selection[key] = 0;
+          }
+          if (key === selected) selection[key]++;
+        });
+        // selected 프로퍼티 삭제
+        delete item.selected;
+
+      });
+
+      result = await StatisticService.addStatistic({parent, mbtiType, mbtiData: createData});
+    }
+    else {
+      // 사실상 싱크 안맞을 위험이 있어서 이렇게 짜면 안됨
+      // 좀 하드코딩임
+      const newSelection = [];
+      const selectedArr = [];
+      mbtiData.forEach(item => {
+        selectedArr.push(item.selected);
+      })
+      // 여기서 seleciton 객체 받아서 selected 에 해당하는거 있으면 값 증가시켜주기
+      stat.mbtiData.forEach((item, i) => {
+        Object.keys(item.selection).forEach(key => {
+          if(key === selectedArr[i]) {
+            item.selection[key]++;
+            item.selection["idx"]= item.idx;
+          }
+        })
+        newSelection.push(item.selection);
+      })
+      result = await StatisticService.updateStatistic({parent, mbtiType, newSelection});
+    }
+
+    res.json(buildResponse(result));
   })
 );
 
