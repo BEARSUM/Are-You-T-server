edf6005713228b46a5939e98a0620aa9fa1d0d03
rebi13 <rebi13@naver.com>
2023-10-01 16:21:38 +0900

    fix: 문항 조회 로직 수정 (순서, 질문 랜덤 조회)

--- src/services/questionService.js
+++ src/services/questionService.js
@@ -7,7 +7,41 @@ class QuestionService {
     this.questionModel = questionModel;
   }
   async getQuestions(parent) {
-    return await this.questionModel.find(parent);
+    const questions = await this.questionModel.find(parent);
+    const selectElements = 2;
+    const selectedQuestions = [];
+
+    // 빈 객체를 초기화하여 값에 따라 요소들을 그룹화합니다.
+    // e, i, s, n, t, f, j, p 에 따라야 됨.
+    const groupedArr = questions.reduce((groups, item) => {
+      if (!groups[item.mbtiType]) {
+        groups[item.mbtiType] = [];
+      }
+      groups[item.mbtiType].push(item);
+      return groups;
+    }, {});
+
+    // 키만 추출하여 배열로 저장합니다.
+    const keys = Object.keys(groupedArr);
+    keys.forEach(key => {
+      // 원본 배열에서 랜덤하게 섞기
+      for (let i = groupedArr[key].length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = groupedArr[key][i];
+        groupedArr[key][i] = groupedArr[key][j];
+        groupedArr[key][j] = temp;
+      }
+      selectedQuestions.push(groupedArr[key].slice(0, selectElements));
+    });
+
+    const randomSeqQuestions = selectedQuestions.flat();
+    for (let i = randomSeqQuestions.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = randomSeqQuestions[i];
+      randomSeqQuestions[i] = randomSeqQuestions[j];
+      randomSeqQuestions[j] = temp;
+    }
+    return randomSeqQuestions;
   }
   async addQuestion(question) {
     return await this.questionModel.create(question);
