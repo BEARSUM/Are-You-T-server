d9b2343ccb4831ef4f1310b4ac1646ce2864608d
rebi13 <rebi13@naver.com>
2023-10-05 23:50:00 +0900

    fix: 게시글 등록/수정 시 비밀번호 추가

--- src/db/models/BoardModel.js
+++ src/db/models/BoardModel.js
@@ -5,20 +5,24 @@ const Board = model('boards', BoardSchema);
 
 class BoardModel {
   async find() {
-    return await Board.find().sort({ createdAt: -1 }).lean();
+    return await Board.find({}, { password: 0 }).sort({ createdAt: -1 }).lean();
   }
   async findMBTI(category) {
-    return await Board.find({ category }).sort({ createdAt: -1 }).lean();
+    return await Board.find({ category }, { password: 0 }).sort({ createdAt: -1 }).lean();
   }
   async findById(id) {
-    return await Board.findById(id).lean();
+    return await Board.findById(id, { password: 0 }).lean();
   }
+  // async findByIdWithPw(id) {
+  //   return await Board.findById(id).lean();
+  // }
   async create(board) {
-    return (await Board.create(board)).toObject();
+    const { hashedPassword, category, title, content, color } = board;
+    return (await Board.create({ password: hashedPassword, category, title, content, color })).toObject();
   }
   async update(id, board) {
-    const { category, title, content, color } = board;
-    return await Board.findByIdAndUpdate(id, { category, title, content, color }, { new: true }).lean();
+    const { hassedPassword, category, title, content, color } = board;
+    return await Board.findByIdAndUpdate(id, { hassedPassword, category, title, content, color }, { new: true }).lean();
   }
   async updateLike(id) {
     return (await Board.findByIdAndUpdate(id, { $inc: { like: 1 } }, { new: true })).toObject();
